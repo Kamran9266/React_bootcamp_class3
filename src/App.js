561c0116afaab5f5051c886b9cf6227322ed1ac1
piaic65617 <fashraf030@gmail.com>
2020-06-12 15:29:03 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App(Props) {
+  return <div>Hello {Props.userName}, Age {Props.age}
+        <h1>PIAIC CNC Specialist</h1>
+        <h2>A four-quarter computing program.</h2>
+        <ol>
+        <hr />
+            <li>Quarter 1: A CNC Foundation.</li>
+            <ul>
+                <li>Introduction to CNC</li>
+                <li>Introduction to Linux and Bash</li>
+                <li>Docker</li>
+                <li>Kubernetes</li>
+            </ul>
+            <hr />
+            <li>Quarter 2: Introduction to Application Development.</li>
+            <ul>
+                <li>GIT</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Javascript</li>
+            </ul>
+            <hr />
+            <li>Quarter 3: Web & Mobile Application Development</li>
+            <ul>
+                <li>Javscript</li>
+                <li>MongoDB</li>
+                <li>Express</li>
+            </ul>
+            <hr />
+            <li>Quarter 4: Advanced Web & Mobile Applicaton Development</li>
+            <ul>
+                <li>React</li>
+                <li>Node.js</li>
+            </ul>
+            <hr />
+        </ol>
+  </div>
 }
-
 export default App;
